49026e72ede8355b39f703b0e64665d1fa46a8eb
root <1158725668@qq.com>
2026-01-02 13:52:27 +0800

    modify cfg80211 Makefile for compile

---
+++ test/cfg80211/regdb.c
@@ -0,0 +1,7 @@
+#include <linux/module.h>
+
+const unsigned char shipped_regdb_certs[] = {};
+const unsigned int shipped_regdb_certs_len = 0;
+
+EXPORT_SYMBOL(shipped_regdb_certs);
+EXPORT_SYMBOL(shipped_regdb_certs_len);
